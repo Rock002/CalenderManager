78533953b458449f263f7ee765371846b2d3bc21
Rock002 <chaykhoyan@mail.ru>
2025-12-06 22:29:50 +0300

    Initial commit

---
+++ src/main/java/com/example/CalenderManager/CalenderManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.CalenderManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalenderManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalenderManagerApplication.class, args);
+	}
+
+}
